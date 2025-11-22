fdb91f307c8d6c3af230f96cc0dba522f48319d6
Leandrus <leandrus.dasilva@gmail.com>
2025-11-22 17:35:10 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+// service-worker.js
+
+const CACHE_NAME = "calculadora-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/manifest.json"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar y limpiar cachés viejas
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Interceptar peticiones y servir desde caché
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
